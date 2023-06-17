a511ea5351b6437b46ea403fee0c591a8aacb471
ketan-sarna <125917404+ketan-sarna@users.noreply.github.com>
2023-06-17 17:43:51 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../App'
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>BookHub&copy made by ketan sarna;</p>
+        <p>BookHub&copy; made by ketan sarna</p>
     </div>
   )
 }
